7b434271e51944f4bb26b6e68908e439e2ad1436
gcosarinsky <gcosarinsky@gmail.com>
2026-03-18 16:07:07 +0100

    add configs folder and CONFIGS_DIR constant

--- inr_apodizations/config.py
+++ inr_apodizations/config.py
@@ -15,9 +15,8 @@ RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 EXTERNAL_DATA_DIR = DATA_DIR / "external"
-
 MODELS_DIR = PROJ_ROOT / "models"
-
+CONFIGS_DIR = PROJ_ROOT / "configs"
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
 
